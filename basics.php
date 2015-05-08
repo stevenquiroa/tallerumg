38140bbbe460835fdd733df2b6693fc897751a95
Steven Quiroa <stevenkm92@gmail.com>
2015-05-08 13:30:04 -0600

    Creamos un post type
    Lo creamos desde el plugin y afecta el funcionamiento de nuestro wordpress y agrega una nueva funcionalidad

--- basics.php
+++ basics.php
@@ -38,3 +38,59 @@ add_filter('login_errors','login_error_message');
 	$error = "Pasword incorrecto, FUERA DE AQUÍ";
 	return $error;
 } 
+
+/**
+* Registers a new post type
+* @uses $wp_post_types Inserts new post type object into the list
+*
+* @param string  Post type key, must not exceed 20 characters
+* @param array|string  See optional args description above.
+* @return object|WP_Error the registered post type object, or an error object
+*/
+function Empresas() {
+
+	$labels = array(
+		'name'                => __( 'Empresas', 'umg' ),
+		'singular_name'       => __( 'Empresa', 'umg' ),
+		'add_new'             => _x( 'Add New Empresa', 'umg', 'umg' ),
+		'add_new_item'        => __( 'Add New Empresa', 'umg' ),
+		'edit_item'           => __( 'Edit Empresa', 'umg' ),
+		'new_item'            => __( 'New Empresa', 'umg' ),
+		'view_item'           => __( 'View Empresa', 'umg' ),
+		'search_items'        => __( 'Search Empresas', 'umg' ),
+		'not_found'           => __( 'No Empresas found', 'umg' ),
+		'not_found_in_trash'  => __( 'No Empresas found in Trash', 'umg' ),
+		'parent_item_colon'   => __( 'Parent Empresa:', 'umg' ),
+		'menu_name'           => __( 'Empresas', 'umg' ),
+	);
+
+	$args = array(
+		'labels'                   => $labels,
+		'hierarchical'        => false,
+		'description'         => 'description',
+		'taxonomies'          => array(),
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => null,
+		'menu_icon'           => null,
+		'show_in_nav_menus'   => true,
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite'             => true,
+		'capability_type'     => 'post',
+		'supports'            => array(
+			'title', 'editor', 'author', 'thumbnail',
+			'excerpt','custom-fields', 'trackbacks', 'comments',
+			'revisions', 'page-attributes', 'post-formats'
+			)
+	);
+
+	register_post_type( 'empresa', $args );
+}
+
+add_action( 'init', 'Empresas' );
