c3fbab8c8c35edb89d918e13d2f2798e471a467b
Steven Quiroa <stevenkm92@gmail.com>
2015-05-08 12:52:36 -0600

    Ya que vimos que utilizamos el action correcto ahora colocamos el codigo verdadero

--- basics.php
+++ basics.php
@@ -10,5 +10,19 @@
 */ 
 add_action( 'wp_head', 'my_facebook_tags' );
 function my_facebook_tags() {
-	echo 'Estoy en la seccion';
+	if( is_single() ) {
+		?>
+		<meta property="og:title" content="<?php the_title() ?>" />
+		<meta property="og:site_name" content="<?php bloginfo( 'name' ) ?>" />
+		<meta property="og:url" content="<?php the_permalink() ?>" />
+		<meta property="og:description" content="<?php the_excerpt() ?>" />
+		<meta property="og:type" content="article" />
+		<?php
+		if ( has_post_thumbnail() ) :
+			$image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+		?>
+		<meta property="og:image" content="<?php echo $image[0]; ?>"/>
+	<?php endif; ?>
+	<?php
+	}
 } 
