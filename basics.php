2a6088835b6f0b742ff19995302337e3c6e2a8d2
Steven Quiroa <stevenkm92@gmail.com>
2015-05-08 12:41:36 -0600

    Agregamos la cabeza del plugin. Esto para identificar nuestro plugins de los demas. Contiene información como:

---
+++ basics.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Mis tags de Facebook
+* Plugin URI: http://nadd.co
+* Description: Este plugin te va a permitir bonitos enlaces en facebook.
+* Version: 1.0.0
+* Author: Steven Quiroa
+* Author URI: http://quiroa.me
+* License: GPL2
+*/ 
