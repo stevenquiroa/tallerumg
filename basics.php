94ea45feb8060373f2041db5012998db1801430f
Steven Quiroa <stevenkm92@gmail.com>
2015-05-08 13:37:13 -0600

    Agregamos una página del plugin

--- basics.php
+++ basics.php
@@ -94,3 +94,12 @@ function Empresas() {
 }
 
 add_action( 'init', 'Empresas' );
+
+add_action('admin_menu', 'my_plugin_menu');
+function my_plugin_menu() {
+	add_menu_page('My Plugin Settings', 'Plugin Settings', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
+}
+function my_plugin_settings_page() {
+	echo "<h2>Plugin page </h2>";
+} 
+
